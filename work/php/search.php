dc6060e9580211405546dd9d1f8497ac743ed145
Lavitz <johan.granberg87@gmail.com>
2020-02-11 15:45:13 +0100

    s05 added searh.php and connection to db

--- work/php/search.php
+++ work/php/search.php
@@ -12,12 +12,8 @@
 
   <div id='page-content'>
     <div class="wrapper">
-
-
       <div id="mydiv">
-
         <div id="duck" class="duck"></div>
-
       </div>
       <!--
   Comments are written as HTML style.
@@ -25,10 +21,74 @@
       <div class="sticky">
       <?php include 'view/header.php'; ?>
       </div>
-
-
           <section class="textblock">
-          
+            <?php
+            /**
+             * A page controller
+             */
+            require "config.php";
+            require "src/functions.php";
+
+            // Get incoming values
+            $search = $_GET["search"] ?? null;
+            $like = "%$search%";
+            //var_dump($_GET);
+
+            if ($search) {
+                // Connect to the database
+                $db = connectDatabase($dsn);
+
+                // Prepare and execute the SQL statement
+                $sql = <<<EOD
+            SELECT
+                *
+            FROM tech
+            WHERE
+                id = ?
+                OR label LIKE ?
+                OR type LIKE ?
+            ;
+            EOD;
+                $stmt = $db->prepare($sql);
+                $stmt->execute([$search, $like, $like]);
+
+                // Get the results as an array with column names as array keys
+                $res = $stmt->fetchAll();
+            }
+
+
+
+
+            ?>
+
+            <h1>Search the database</h1>
+
+            <form>
+                <p>
+                    <label>Search:
+                        <input type="text" name="search" value="<?= $search ?>">
+                    </label>
+                </p>
+            </form>
+        
+            <?php if ($search) : ?>
+                <table>
+                    <tr>
+                        <th>Label</th>
+                        <th>Type</th>
+                    </tr>
+
+                <?php foreach ($res as $row) : ?>
+                    <tr>
+                        <td><?= $row["id"] ?></td>
+                        <td><?= $row["label"] ?></td>
+                        <td><?= $row["type"] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+
+                </table>
+            <?php endif; ?>
+
           </section>
       <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/all.js" integrity="sha384-xymdQtn1n3lH2wcu0qhcdaOpQwyoarkgLVxC/wZ5q7h9gHtxICrpcaSUfygqZGOe" crossorigin="anonymous"></script>
     </div>

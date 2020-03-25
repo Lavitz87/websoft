105d10e07ac4c441a7ac8f270cfd32678c182ee5
Lavitz <johan.granberg87@gmail.com>
2020-03-25 13:51:25 +0100

    Part 10

--- work/report/js/school.js
+++ work/report/js/school.js
@@ -1,62 +1,62 @@
+(function() {
 
-(function () {
 
-
-console.log('Sandbox MEGA is ready!');
+    console.log('Sandbox MEGA is ready!');
 })();
 
 
-function fetchScools(){
+function fetchScools() {
 
-  'use strict';
-
-
-  fetch('data/1081.json')
-  .then((response) => {
-      return response.json();
-  })
-  .then((myJson) => {
-
-    var school = myJson.Skolenheter;
-    var col = [];
-       for (var i = 0; i < school.length; i++) {
-           for (var key in school[i]) {
-               if (col.indexOf(key) === -1) {
-                   col.push(key);
-               }
-           }
-       }
-
-       var table = document.createElement("table");
-
-              // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-
-              var tr = table.insertRow(-1);                   // TABLE ROW.
-
-              for (var i = 0; i < col.length; i++) {
-                  var th = document.createElement("th");      // TABLE HEADER.
-                  th.innerHTML = col[i];
-                  tr.appendChild(th);
-              }
-
-              // ADD JSON DATA TO THE TABLE AS ROWS.
-              for (var i = 0; i < school.length; i++) {
-
-                  tr = table.insertRow(-1);
-
-                  for (var j = 0; j < col.length; j++) {
-                      var tabCell = tr.insertCell(-1);
-                      tabCell.innerHTML = school[i][col[j]];
-                  }
-              }
-
-              // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-              var divContainer = document.getElementById("showData");
-              divContainer.innerHTML = "";
-              divContainer.appendChild(table);
+    'use strict';
 
 
-      console.log(myJson);
-  });
+    fetch('data/1081.json')
+        .then((response) => {
+            return response.json();
+        })
+        .then((myJson) => {
+
+            var school = myJson.Skolenheter;
+            var col = [];
+            for (var i = 0; i < school.length; i++) {
+                for (var key in school[i]) {
+                    if (col.indexOf(key) === -1) {
+                        col.push(key);
+                    }
+                }
+            }
+
+            var table = document.createElement("table");
+
+
+            // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+            var tr = table.insertRow(-1); // TABLE ROW.
+
+            for (var i = 0; i < col.length; i++) {
+                var th = document.createElement("th"); // TABLE HEADER.
+                th.innerHTML = col[i];
+                tr.appendChild(th);
+            }
+
+            // ADD JSON DATA TO THE TABLE AS ROWS.
+            for (var i = 0; i < school.length; i++) {
+
+                tr = table.insertRow(-1);
+
+                for (var j = 0; j < col.length; j++) {
+                    var tabCell = tr.insertCell(-1);
+                    tabCell.innerHTML = school[i][col[j]];
+                }
+            }
+
+            // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+            var divContainer = document.getElementById("showData");
+            divContainer.innerHTML = "";
+            divContainer.appendChild(table);
+
+
+            console.log(myJson);
+        });
 
 }
